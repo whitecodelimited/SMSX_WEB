dfd4ceba9150915571674963ff2958b58d2b8a7c
whitecodelimited <whitecodelimited@gmail.com>
2026-07-09 00:37:35 +0300

    Fix support admin base path

--- support-admin/vite.config.js
+++ support-admin/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  base: "/support-admin/",
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    base: env.VITE_APP_BASE_PATH || "/",
+    plugins: [react()],
+  };
 });
